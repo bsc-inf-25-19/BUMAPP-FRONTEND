68cedd56d976b740f09af797dd3d82d1d0f80750
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-02-23 20:53:43 +0200

    connect with Addproduct page

--- src/views/Product/Product.vue
+++ src/views/Product/Product.vue
@@ -3,16 +3,17 @@
     <div class="row">
       <div class="col-12 text-center">
         <h3 class="pt-3">Products</h3>
+        <router-link 
+        :to="{ name: 'AddProduct' }">
+        <button class=" btn" style="float:right">Add new product</button>
+        </router-link>
       </div>
     </div>
     <div class="row">
-      <div v-for ="product of products" 
-      :key="product.id"
-      class="col-xl-4 col-md-6 d-flex ">
-      <ProductBox :product="product"> </ProductBox>
+      <div v-for="product of products" :key="product.id" class="col-xl-4 col-md-6 d-flex">
+        <ProductBox :product="product"></ProductBox>
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -20,24 +21,24 @@
 const axios = require("axios");
 import ProductBox from "../../components/Product/ProductBox.vue";
 export default {
-  name : "Product",
-  components : {ProductBox},
-    data() {
-        return {
-            baseURL: "https://fast-fortress-80573.herokuapp.com",
-            products: []
-        };
-    },
-    methods: {
-        async getProducts() {
-            await axios.get(`${this.baseURL}/product/list`)
-                .then(res => this.products = res.data)
-                .catch(err => console.log(err));
-        }
-    },
-    mounted() {
-        this.getProducts();
+  name: "Product",
+  components: { ProductBox },
+  data() {
+    return {
+      baseURL: "https://fast-fortress-80573.herokuapp.com",
+      products: []
+    };
+  },
+  methods: {
+    async getProducts() {
+      await axios.get(`${this.baseURL}/product/list`)
+        .then(res => this.products = res.data)
+        .catch(err => console.log(err));
     }
+  },
+  mounted() {
+    this.getProducts();
+  }
 };
 
 </script>
