9ac183dbfed06bfff2c2e0948b11627422a481a7
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-02-24 03:34:36 +0200

    admin panel

---
+++ src/views/Admin.vue
@@ -0,0 +1,14 @@
+<template>
+    <router-link :to="{ name: 'Product' }">
+        <button type="button" class="btn btn-primary btn-lg">Products in Database</button>
+    </router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
