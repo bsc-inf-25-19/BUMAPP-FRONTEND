2898144466b66f5ca3e1f08932236d3d8856584e
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-02-24 03:34:01 +0200

    imprt admin panel

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AddProduct from '../views/Product/AddProduct.vue'
 import Product from '../views/Product/Product.vue'
+import Admin from  '../views/Admin.vue'
 
 
 
@@ -18,6 +19,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
   },
   {
     path: '/admin/product/add',
