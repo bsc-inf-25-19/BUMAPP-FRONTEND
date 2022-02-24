0666e36c1ac88a78d848107061d9811d8e09a0e9
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-02-24 21:04:59 +0200

    add product page

---
+++ src/views/Product/EditProduct.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h4 class="pt-3">Edit Product</h4>
+            </div>
+        </div>
+        <div class="row">{{product}}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            product: null,
+            id: null
+        }
+    },
+    props: ["baseURL", "products"],
+    mounted() {
+        this.id = this.$route.params.id;
+        this.product = this.products.find(product => product.id == this.id);
+        console.log('product', this.product);
+    }
+
+}
+</script>
+
+<style>
+</style>
