07737875ba528543dbbfc807660fbad8105a8b5e
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-02-26 01:10:42 +0200

    update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,60 @@
-<template>
-  <div class="home">
-    home page bro
+ <template>
+  <div id="home">
+    <div id="background-div" class="page-holder bg-cover">
+      <div class="container py-6">
+        <header class="text-left text-white py-5">
+          <h3 class="mb-4 rounded" id="heading">
+            <a href="#start-shopping" class="bg-white px-2 py-2 rounded"
+              >Start shopping</a
+            >
+          </h3>
+          <p class="lead mb-0 bg-dark p-1 rounded">Bumapp</p>
+        </header>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h2 class="pt-3">Top Products</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          v-for="index in this.productSize"
+          :key="index"
+          class="col-md-6 col-xl-4 col-12 pt-3 justify-content-around d-flex"
+        >
+          <ProductBox :product="products[index - 1]"></ProductBox>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import ProductBox from "../components/Product/ProductBox.vue";
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  name: "Home",
+  components: { ProductBox },
+  props: ["products"],
+  data() {
+    return {
+      productSize: 0,
+    };
+  },
+  mounted() {
+    this.productSize = Math.min(6, this.products.length);
+  },
+};
 </script>
+<style>
+.bg-cover {
+  background-size: cover !important;
+}
+.page-holder {
+  min-height: 100vh;
+}
+#background-div {
+  background: url("../assets/home-first.jpg");
+}
+</style>
