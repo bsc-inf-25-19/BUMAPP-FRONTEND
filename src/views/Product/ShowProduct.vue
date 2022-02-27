b21f080d35f5f0dcf6075cdf0c1d97ca3567fd9d
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-02-27 19:25:22 +0200

    add product page for normal user

---
+++ src/views/Product/ShowProduct.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="row pt-5">
+      <div class="col-md-1"></div>
+      <div class="col-md-4 col-12">
+        <img :src="product.imgURL" :alt="product.name" class="img-fluid" />
+      </div>
+      <div class="col-md-6 col-12 pt-3 pt-md-0">
+        <h4>{{ product.name }}</h4>
+        <h6 class="font-weight-bold">MWK {{ product.price }}</h6>
+        <p>{{ product.description }}</p>
+
+        <div class="d-flex flex-row justify-content-between">
+          <div class="input-group col-md-3 col-4 p-0">
+            <button
+              type="button"
+              id="add-to-cart-button"
+              style="float: right"
+              class="btn btn-outline-primary"
+            >
+              Add to Cart
+              <ion-icon name="cart-outline" v-pre></ion-icon>
+            </button>
+          </div>
+        </div>
+
+        <div class="features pt-3">
+          <h5>
+            <strong>Features</strong>
+          </h5>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Officia quas, officiis eius magni error magnam voluptatem</li>
+            <li>nesciunt quod! Earum voluptatibus quaerat dolorem doloribus</li>
+            <li>molestias ipsum ab, ipsa consectetur laboriosam soluta et</li>
+            <li>ut doloremque dolore corrupti, architecto iusto beatae.</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-1"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: {},
+      id: null,
+    };
+  },
+  props: ["baseURL", "products"],
+  methods: {},
+  mounted() {
+    this.id = this.$route.params.id;
+    this.product = this.products.find((product) => product.id == this.id);
+  },
+};
+</script>
+
+<style>
+</style>
